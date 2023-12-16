e3e46c005b9ff91411645816880284316ab4052b
Robert Lippmann <rlippmann@hotmail.com>
2023-12-15 21:35:17 -0500

    break site properties out from site

--- pyadtpulse/site.py
+++ pyadtpulse/site.py
@@ -3,24 +3,15 @@ import logging
 import re
 from asyncio import Task, create_task, gather, get_event_loop, run_coroutine_threadsafe
 from datetime import datetime
-from threading import RLock
 from time import time
-from warnings import warn
 
 from bs4 import BeautifulSoup, ResultSet
 from typeguard import typechecked
 
-from .alarm_panel import ADTPulseAlarmPanel
 from .const import ADT_DEVICE_URI, ADT_GATEWAY_STRING, ADT_GATEWAY_URI, ADT_SYSTEM_URI
-from .gateway import ADTPulseGateway
 from .pulse_connection import PulseConnection
-from .util import (
-    DebugRLock,
-    make_soup,
-    parse_pulse_datetime,
-    remove_prefix,
-    set_debug_lock,
-)
+from .site_properties import ADTPulseSiteProperties
+from .util import make_soup, parse_pulse_datetime, remove_prefix
 from .zones import ADTPulseFlattendZone, ADTPulseZones
 
 LOG = logging.getLogger(__name__)
@@ -29,19 +20,10 @@ SECURITY_PANEL_ID = "1"
 SECURITY_PANEL_NAME = "Security Panel"
 
 
-class ADTPulseSite:
+class ADTPulseSite(ADTPulseSiteProperties):
     """Represents an individual ADT Pulse site."""
 
-    __slots__ = (
-        "_pulse_connection",
-        "_id",
-        "_name",
-        "_last_updated",
-        "_alarm_panel",
-        "_zones",
-        "_site_lock",
-        "_gateway",
-    )
+    __slots__ = ("_pulse_connection",)
 
     @typechecked
     def __init__(self, pulse_connection: PulseConnection, site_id: str, name: str):
@@ -53,58 +35,7 @@ class ADTPulseSite:
             name (str): site name
         """
         self._pulse_connection = pulse_connection
-        self._id = site_id
-        self._name = name
-        self._last_updated: int = 0
-        self._zones = ADTPulseZones()
-        self._site_lock: RLock | DebugRLock
-        self._site_lock = set_debug_lock(
-            self._pulse_connection.debug_locks, "pyadtpulse.site_lock"
-        )
-        self._alarm_panel = ADTPulseAlarmPanel()
-        self._gateway = ADTPulseGateway()
-
-    @property
-    def id(self) -> str:
-        """Get site id.
-
-        Returns:
-            str: the site id
-        """
-        return self._id
-
-    @property
-    def name(self) -> str:
-        """Get site name.
-
-        Returns:
-            str: the site name
-        """
-        return self._name
-
-    # FIXME: should this actually return if the alarm is going off!?  How do we
-    # return state that shows the site is compromised??
-
-    @property
-    def last_updated(self) -> int:
-        """Return time site last updated.
-
-        Returns:
-            int: the time site last updated as datetime
-        """
-        with self._site_lock:
-            return self._last_updated
-
-    @property
-    def site_lock(self) -> "RLock| DebugRLock":
-        """Get thread lock for site data.
-
-        Not needed for async
-
-        Returns:
-            RLock: thread RLock
-        """
-        return self._site_lock
+        super().__init__(site_id, name, pulse_connection.debug_locks)
 
     @typechecked
     def arm_home(self, force_arm: bool = False) -> bool:
@@ -141,53 +72,6 @@ class ADTPulseSite:
     async def async_disarm(self) -> bool:
         """Disarm system async."""
         return await self.alarm_control_panel.async_disarm(self._pulse_connection)
-
-    @property
-    def zones(self) -> list[ADTPulseFlattendZone] | None:
-        """Return all zones registered with the ADT Pulse account.
-
-        (cached copy of last fetch)
-        See Also fetch_zones()
-        """
-        with self._site_lock:
-            if not self._zones:
-                raise RuntimeError("No zones exist")
-            return self._zones.flatten()
-
-    @property
-    def zones_as_dict(self) -> ADTPulseZones | None:
-        """Return zone information in dictionary form.
-
-        Returns:
-            ADTPulseZones: all zone information
-        """
-        with self._site_lock:
-            if not self._zones:
-                raise RuntimeError("No zones exist")
-            return self._zones
-
-    @property
-    def alarm_control_panel(self) -> ADTPulseAlarmPanel:
-        """Return the alarm panel object for the site.
-
-        Returns:
-            Optional[ADTPulseAlarmPanel]: the alarm panel object
-        """
-        return self._alarm_panel
-
-    @property
-    def gateway(self) -> ADTPulseGateway:
-        """Get gateway device object.
-
-        Returns:
-            ADTPulseGateway: Gateway device
-        """
-        return self._gateway
-
-    @property
-    def history(self):
-        """Return log of history for this zone (NOT IMPLEMENTED)."""
-        raise NotImplementedError
 
         #        status_orb = summary_html_soup.find('canvas', {'id': 'ic_orb'})
         #        if status_orb:
@@ -508,47 +392,3 @@ class ADTPulseSite:
         """
         coro = self._async_update_zones()
         return run_coroutine_threadsafe(coro, get_event_loop()).result()
-
-    @property
-    def updates_may_exist(self) -> bool:
-        """Query whether updated sensor data exists.
-
-        Deprecated, use method on pyADTPulse object instead
-        """
-        # FIXME: this should actually capture the latest version
-        # and compare if different!!!
-        # ...this doesn't actually work if other components are also checking
-        #  if updates exist
-        warn(
-            "updates_may_exist on site object is deprecated, "
-            "use method on pyADTPulse object instead",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return False
-
-    async def async_update(self) -> bool:
-        """Force update site/zone data async with current data.
-
-        Deprecated, use method on pyADTPulse object instead
-        """
-        warn(
-            "updating zones from site object is deprecated, "
-            "use method on pyADTPulse object instead",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return False
-
-    def update(self) -> bool:
-        """Force update site/zones with current data.
-
-        Deprecated, use method on pyADTPulse object instead
-        """
-        warn(
-            "updating zones from site object is deprecated, "
-            "use method on pyADTPulse object instead",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return False
