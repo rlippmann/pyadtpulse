0738051f8e4acefb1354f0cfb1039621df02fbb1
Robert Lippmann <rlippmann@hotmail.com>
2023-12-19 00:56:33 -0500

    restrict update_zones_from soup to orb div in html

--- pyadtpulse/site.py
+++ pyadtpulse/site.py
@@ -292,7 +292,25 @@ class ADTPulseSite(ADTPulseSiteProperties):
         # parse ADT's convulated html to get sensor status
         with self._site_lock:
             gateway_online = False
-            for row in soup.find_all("tr", {"class": "p_listRow"}):
+            orbsensors = soup.find("div", id="orbSensorsList")
+            if not orbsensors:
+                return None
+            for row in orbsensors.find_all("tr", {"class": "p_listRow"}):
+                temp = row.find("div", {"class": "p_grayNormalText"})
+                # v26 and lower: temp = row.find("span", {"class": "p_grayNormalText"})
+                if temp is None:
+                    break
+                try:
+                    zone = int(
+                        remove_prefix(
+                            temp.get_text(),
+                            "Zone\xa0",
+                        )
+                    )
+                except ValueError:
+                    LOG.debug("skipping row due to zone not being an integer")
+                    continue
+                # parse out last activity (required dealing with "Yesterday 1:52 PM")
                 temp = row.find("span", {"class": "devStatIcon"})
                 if temp is None:
                     break
@@ -304,16 +322,7 @@ class ADTPulseSite(ADTPulseSiteProperties):
                 except ValueError:
                     last_update = datetime(1970, 1, 1)
                 # name = row.find("a", {"class": "p_deviceNameText"}).get_text()
-                temp = row.find("div", {"class": "p_grayNormalText"})
-                # v26 and lower: temp = row.find("span", {"class": "p_grayNormalText"})
-                if temp is None:
-                    break
-                zone = int(
-                    remove_prefix(
-                        temp.get_text(),
-                        "Zone\xa0",
-                    )
-                )
+
                 state = remove_prefix(
                     row.find("canvas", {"class": "p_ic_icon_device"}).get("icon"),
                     "devStat",
@@ -335,9 +344,6 @@ class ADTPulseSite(ADTPulseSiteProperties):
                                 status = "Unknown trouble code"
                         else:
                             status = "Online"
-
-                # parse out last activity (required dealing with "Yesterday 1:52 PM")
-                #           last_activity = time.time()
 
                 # id:    [integer]
                 # name:  device name
